0be1612fd0098d7188d1b4cb270aece4bef04527
aimalkhanofficial <aimalkhanofficial@gmail.com>
2024-04-22 10:59:14 -0400

    worked on 2 - passes about 41 of 124 test cases

--- 2-GreatestCommonDivisor.js
+++ 2-GreatestCommonDivisor.js
@@ -3,15 +3,38 @@
  * @param {string} str2
  * @return {string}
  */
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
 var gcdOfStrings = function(str1, str2) {
-    let copyOfStr1 = str1.split(str2);
-    while (copyOfStr1.length > 1 && copyOfStr1.length < str2.length) {
-        if (copyOfStr1.includes(str2)) {
-            let index = copyOfStr1.indexOf(str2);
-            copyOfStr1.splice(index);
+    let reverseCounter = str2.length;
+    let wordToTry = '';
+    let fullMatchFound = false;
+    for (let i = 0; i < str2.length; i++) {
+        wordToTry = str2.substring(reverseCounter, 0);
+        //console.log(wordToTry)
+        let copyOfStr1 = str1;
+        while (!fullMatchFound) {
+            //console.log(copyOfStr1, wordToTry, copyOfStr1.includes(wordToTry), copyOfStr1.length);
+            if (copyOfStr1.includes(wordToTry)) {
+                copyOfStr1 = copyOfStr1.replace(wordToTry, '')
+            }
+            else if (copyOfStr1.length === wordToTry.length && !copyOfStr1.includes(wordToTry)) {
+                fullMatchFound = false;
+                //console.log('breaking loop on 2')
+                break;
+            }
+            else if (copyOfStr1.length === 0){
+                fullMatchFound = true;
+                //console.log('zeroooooo')
+                break;
+            }
+            else if (copyOfStr1.length < wordToTry.length || copyOfStr1.length > wordToTry.length) {
+                fullMatchFound = false;
+                //console.log('breaking loop on 3')
+                break;
+            }
         }
+        if (fullMatchFound) break;
+        reverseCounter--;
     }
-    return 'x';
+    return fullMatchFound ? wordToTry : '';
 };
-
-console.log('>>> ABCABC', 'ABCABC', 'ABC')
