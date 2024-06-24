882cde12d4b798a0e08a4e852377d90a5d6ae301
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-06-23 23:13:58 -0400

    converting to decimals first and then to binary

--- 31-AddBinary.js
+++ 31-AddBinary.js
@@ -4,57 +4,32 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    let sum = '';
-    let carryOver = '';
+    if (a === 0 && b === 0) return 0;
+    let aInDecimal = 0;
+    let bInDecimal = 0;
+    let sum = 0;
+    let counter = 0;
     for (let i = a.length - 1; i >= 0; i--) {
-        if (a[i] && b[i]) {
-            if (a[i] === '0' && b[i] === '0') {
-                if (carryOver === '1') {
-                    sum += '1';
-                    carryOver = '0';
-                } else {
-                    sum += '0';
-                }
-                console.log('>> adding', a[i], '+', b[i], sum, '. CarryOver:', carryOver)
-            }
-            else if (a[i] === '0' && b[i] === '1') {
-                if (carryOver === '1') {
-                    sum += '0';
-                    carryOver = '0';
-                } else {
-                    sum += '1';
-                }
-                console.log('>> adding', a[i], '+', b[i], sum, '. CarryOver:', carryOver)
-            }
-            else if (a[i] === '1' && b[i] === '0') {
-                if (carryOver === '1') {
-                    sum += '0';
-                    carryOver = '0';
-                } else {
-                    sum += '0';
-                }
-                console.log('>> adding', a[i], '+', b[i], sum, '. CarryOver:', carryOver)
-            }
-            else if (a[i] === '1' && b[i] === '1') {
-                if (carryOver === '1') {
-                    sum += '1';
-                    carryOver = '1';
-                } else {
-                    sum += '0';
-                    carryOver = '1';
-                }
-                console.log('>> adding', a[i], '+', b[i], sum, '. CarryOver:', carryOver)
-            }
-        }
-        else if (!b[i]) {
-            //console.log('>> sum', sum, a[i])
-            sum += a[i];
-            console.log('>> here adding', a[i], '+', b[i], sum, 'CarryOver:', carryOver)
-        }
+        aInDecimal += (a[i] * Math.pow(2, counter));
+        counter++;
     }
-    sum = sum + carryOver;
-    console.log('>> final:', sum);
-    return [...sum].reverse().join('');
+    counter = 0;
+    for (let i = b.length - 1; i >= 0; i--) {
+        bInDecimal += (b[i] * Math.pow(2, counter));
+        counter++;
+    }
+    sum = aInDecimal + bInDecimal;
+    //console.log('>> aInDecimal', aInDecimal, bInDecimal, sum);
+
+    let remainder = 0;
+    let binary = '';
+    while (sum >= 1) {
+        remainder = sum % 2;
+        sum = (sum - remainder) / 2;
+        binary += remainder;
+    }
+    //console.log('>> aInDecimal', aInDecimal, bInDecimal, sum, binary);
+    return [...binary].reverse().join('');
 };
-//console.log('>>> 1010, 1011', addBinary('1010', '1011'))
+console.log('>>> 1010, 1011', addBinary('1010', '1011'))
 console.log('>>> 11, 1', addBinary('11', '1'))
