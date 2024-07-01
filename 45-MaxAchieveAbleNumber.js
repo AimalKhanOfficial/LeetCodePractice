999cc47768cf89fadf4a748bef60752f4854fb05
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-07-01 11:47:11 -0400

    easy LC

---
+++ 45-MaxAchieveAbleNumber.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+//Solved - https://leetcode.com/problems/find-the-maximum-achievable-number/submissions/1306029626/
+var theMaximumAchievableX = function(num, t) {
+    return num + t + t;
+};
