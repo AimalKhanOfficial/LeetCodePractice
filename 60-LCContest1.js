acdd149272316a8f204fdde5e9b7b44a066990ba
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-07-06 10:57:58 -0400

    Medium -  531 / 625 testcases passed

--- 60-LCContest1.js
+++ 60-LCContest1.js
@@ -1,18 +1,16 @@
-
-
-
-//https://leetcode.com/contest/biweekly-contest-134/problems/alternating-groups-i/submissions/1311634998/
+//Q1 - https://leetcode.com/contest/biweekly-contest-134/problems/alternating-groups-i/submissions/1311634998/
+//Q2 - https://leetcode.com/contest/biweekly-contest-134/problems/alternating-groups-ii/submissions/1311681412/
 /**
  * @param {number[]} colors
  * @return {number}
  */
-var numberOfAlternatingGroups = function(colors) {
+var numberOfAlternatingGroups = function(colors, k) {
     if (!colors) return colors;
     let allPossibilities = [];
     for (let i = 0; i < colors.length; i++) {
         let counter = i;
         let possibility = [];
-        for (let j = 0; j < 3; j++) {
+        for (let j = 0; j < k; j++) {
             if (counter >= colors.length) {
                 counter = 0;
             }
@@ -21,9 +19,26 @@ var numberOfAlternatingGroups = function(colors) {
         }
         allPossibilities.push(possibility)
     }
-    allPossibilities = allPossibilities.filter(possibility => possibility[1] !== possibility[0] && possibility[1] !== possibility[2])
+    allPossibilities = allPossibilities.filter(possibility => {
+        for (let i = 0; i < possibility.length; i++) {
+            if (i === 0 || i === possibility.length) continue;
+            if (i + 1 === possibility.length) break;
+            if (possibility[i] !== possibility[i - 1] && possibility[i] !== possibility[i + 1]) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    })
+    //console.log('>> allPossibilities', allPossibilities)
     return allPossibilities.length;
 };
 
-console.log('>> [0,1,0,0,1]', numberOfAlternatingGroups([0,1,0,0,1]))
-console.log('>> [1,1,1]', numberOfAlternatingGroups([1,1,1]))
+//Q#2
+console.log('>> [0,1,0,0,1]', numberOfAlternatingGroups([0,1,0,1,0], 3))
+console.log('>> [0,1,0,0,1,0,1]', numberOfAlternatingGroups([0,1,0,0,1,0,1], 6))
+console.log('>> [1,1,0,1]', numberOfAlternatingGroups([1,1,0,1], 4))
+
+//Q#1
+//console.log('>> [1,1,1]', numberOfAlternatingGroups([1,1,1]))
