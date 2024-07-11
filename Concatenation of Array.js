aed712a64bb68ce98a9dcc7d181c389f7aa5324f
Aimal Khan <36460898+AimalKhanOfficial@users.noreply.github.com>
2024-07-11 14:44:38 -0400

    Create Concatenation of Array.js

---
+++ Concatenation of Array.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/concatenation-of-array/submissions/1317920107/
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    return [celsius + 273.15, celsius * 1.80 + 32.00]
+};
