53fce65ec12b1341900f9193093eb0285a2980a0
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-09-24 07:35:44 -0400

    Solved with modulo

--- 03-Sept-2024/13-Find the Encrypted String.js
+++ 03-Sept-2024/13-Find the Encrypted String.js
@@ -3,11 +3,14 @@
  * @param {number} k
  * @return {string}
  */
+
+//Solved - https://leetcode.com/problems/find-the-encrypted-string/submissions/1400634977/
+
 var getEncryptedString = function(s, k) {
     let final = [];
     //For each character c in s, replace c with the kth character after c in the string (in a cyclic manner).
     for (let i = 0; i < s.length; i++) {
-        let indexToReplaceWith = (i + k) >= s.length ? (i + k) - s.length : i + k;
+        let indexToReplaceWith = (i + k) % s.length;
         final.push(s[indexToReplaceWith]);
     }
     return final.join('');
