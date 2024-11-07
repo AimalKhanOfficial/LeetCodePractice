b2f1685c4fb31713e12f69a092063d11fccd5fc7
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-11-06 23:15:25 -0500

    LC Easy - SQL Query to get avg exp of Project Employees

---
+++ 05-Nov-2024/04-Project Employees I.sql
@@ -0,0 +1,4 @@
+select p.project_id, ROUND(sum(e.experience_years)/count(e.experience_years), 2) as `average_years`
+from project p join employee e 
+on p.employee_id = e.employee_id
+group by p.project_id
