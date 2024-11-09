b08b2607ff9320e02cb8c3df7d5b64b70c3293dc
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-11-09 06:52:16 -0500

    Max Production Diff Solved - Improved Solution

--- 05-Nov-2024/07-Maximum Product Difference Between Two Pairs.js
+++ 05-Nov-2024/07-Maximum Product Difference Between Two Pairs.js
@@ -4,23 +4,28 @@
  */
 var maxProductDifference = function(nums) {
 
-    const pairs = [];
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            pairs.push(nums[i] * nums[j]);
-        }
-    }
+    nums = nums.sort((a, b) => b - a);
 
-    let maxDifferences = [];
-    for (let i = 0; i < pairs.length; i++) {
-        for (let j = i + 1; j < pairs.length; j++) {
-            maxDifferences.push(Math.abs(pairs[i] - pairs[j]));
+    if (nums.length > 4) {
+        let lastIndex = nums.length - 1;
+        return (nums[0] * nums[1]) -  (nums[lastIndex - 1] * nums[lastIndex])
+    } else {
+        const pairs = [];
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                pairs.push(nums[i] * nums[j]);
+            }
         }
-    }
 
-    maxDifferences = maxDifferences.sort((a, b) => b - a);
-    
-    return maxDifferences[0];
+        const maxDifferences = [];
+        for (let i = 0; i < pairs.length; i++) {
+            for (let j = i + 1; j < pairs.length; j++) {
+                maxDifferences.push(Math.abs(pairs[i] - pairs[j]));
+            }
+        }
+        
+        return Math.max(...maxDifferences);
+    }
 };
 
 console.log('>> ', maxProductDifference(nums = [4,2,5,9,7,4,8]))
